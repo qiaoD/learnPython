17e90ce4626136f839d66b362b5c1308ac6eaaf6
QD <QD@cug.edu.cn>
2018-02-09 21:50:53 +0800

    edit cmp.py

--- cmp.py
+++ cmp.py
@@ -10,27 +10,32 @@ info   : Context-Management-Protocol
 '''
 
 class MyCMP:
-    def __init__(self, tag=""):
+    def __init__(self, tag):
         self.tag = tag
-        print('Resource is [%s]' % self.tag)
-        
+        print("[__init__]:%s" % tag)
+		
     def __enter__(self):
-        print('[__enter__():%s]:Allocate the Resource' % self.tag)
-        return self
-    
+        print("[__enter__]:%s" % self.tag)
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print('[__exit__():%s]:free the Resource' % self.tag)
+
         if exc_tb is None:
-            print('[__exit__():%s]:without exception' % self.tag)
+            print("[__exit__]: %s" % self.tag)
         else:
-            print('[__exit__():%s]:with exception' % self.tag)
-            return False
+            print("exc_type :%s" % exc_type )
+            print("exc_val  :%s" % exc_val)
+            print("exc_tb   :%s" % exc_tb)
+            return 1
+            
 
-with MyCMP('qiao'):
-    print('load MyCMP')
+with MyCMP('normal'):
+    print('with-body')
 
+print('================================')
 
 with MyCMP('with-exception'):
+    print('with-body')
     raise Exception
+
+
 
 
